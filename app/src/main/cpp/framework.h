f9f83b50dbbe41b8d7c4941461a0c6421917e0ea
vyu06133 <vyu06133@gmail.com>
2025-05-26 15:40:31 +0900

    更新

---
+++ app/src/main/cpp/framework.h
@@ -0,0 +1,35 @@
+//
+// Created by vyu06133 on 2025/05/25 日.
+//
+
+#pragma once
+
+#include <algorithm> // for sort
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdarg> // for va_list
+#include <cassert>
+#include <cstring>
+#include <exception>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+#include <vector>
+#include <array>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <functional>
+
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+using glm::mat4;
+using glm::vec3;
+
+
